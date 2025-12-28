566018bf27e465e671f9be5c0d04c19ef7b3ec38
Shankar-sala <Shankarsala9995@gmail.com>
2025-12-28 22:12:00 +0530

    messageRoutes & userRoutes completed

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,5 +1,5 @@
 import fs from "fs";
-import imagekit from "../configs/imageKit";
+import imagekit from "../configs/imageKit.js";
 import Message from "../models/Message.js";
 //Create an empty object to store SS Event connections
 const connections = {};
@@ -66,13 +66,53 @@ export const sendMessage = async (req, res) => {
     res.json({ success: true, message });
 
     //Send message to to_user_id using SSE
+    const messageWithUserData = await Message.findById(message._id).populate(
+      "from_user_id"
+    );
 
-  //   const messageWithUserData = await Message.findById(message._id).populate("from_user_id");
-
-  //   if(connections[to_user_id]){
-  //     connections[to_user_id].write(`data: ${JSON.stringify(messageWithUserData)\n\n}`)
-  //   }
+    if (connections[to_user_id]) {
+      connections[to_user_id].write(
+        `data: ${JSON.stringify(messageWithUserData)}\n\n`
+      );
+    }
   } catch (error) {}
-  console.log(error)
-  res.json({success: false, message: error.message})
+  console.log(error);
+  res.json({ success: false, message: error.message });
+};
+
+// Get Chat Messages
+export const getChatMessages = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const { to_user_id } = req.body;
+
+    const messages = await Message.find({
+      $or: [
+        { from_user_id: userId, to_user_id },
+        { from_user_id: to_user_id, to_user_id: userId },
+      ],
+    }).sort({ created_at: -1 });
+
+    //mark messages as seen
+    await Message.updateMany(
+      { from_user_id: to_user_id, to_user_id: userId },
+      { seen: true }
+    );
+
+    res.json({ success: true, messages });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// Get Recent Messages
+export const getUserRecentMessages = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const messages = await Message.find({to_user_id: userId}.populate('from_user_id to_user_id')).sort({ created_at: -1 });
+
+     res.json({ success: true, messages });
+  } catch (error) {
+     res.json({ success: false, message: error.message });
+  }
 }
