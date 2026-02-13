88fb47c47b69a97d62f476276d11ed177fa8c1a0
Shankar-Sala <shankarsala9995@gmail.com>
2026-02-13 19:54:44 +0530

    db connected

---
+++ backend-server/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./configs/db.js";
+
+const app = express();
+
+await connectDB();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => res.send("✅ Server is running"));
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () =>
+  console.log(`🚀 Server running on http://localhost:${PORT}`)
+);
