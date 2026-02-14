e71833b09897c424c653425da445d936b84dcf37
Shankar-Sala <shankarsala9995@gmail.com>
2026-02-14 21:01:09 +0530

    userRoute Completed

---
+++ backend-server/routes/userRoutes.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { discoverUsers, followUser, getUserData, unfollowUser } from "../controllers/userController";
+import { protect } from "../middlewares/auth";
+import { upload } from "../configs/multer";
+
+
+const userRouter = express.Router();
+
+userRouter.get("/data", protect, getUserData);
+userRouter.post("/update", upload.fields([{ name: 'profile', maxCount: 1 }, { name: 'cover', maxCount: 1 }]), protect, updateUserData);
+userRouter.post("/discover", protect, discoverUsers); 
+userRouter.post("/follow", protect, followUser);
+userRouter.post("/unfollow", protect, unfollowUser);
+
+export default userRouter;
