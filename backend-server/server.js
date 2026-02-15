f21a21e2213824bd8f6abe4f957e69f030477d8f
Shankar-Sala <shankarsala9995@gmail.com>
2026-02-16 04:22:00 +0530

    postController setup Completed

--- backend-server/server.js
+++ backend-server/server.js
@@ -6,6 +6,7 @@ import { inngest, functions } from "./inngest/index.js";
 import { serve } from "inngest/express";
 import { clerkMiddleware } from '@clerk/express'
 import userRouter from "./routes/userRoutes.js";
+import postRouter from "./routes/postRoutes.js";
 
 
 const app = express();
@@ -18,7 +19,8 @@ app.use(clerkMiddleware())
 
 app.get("/", (req, res) => res.send("✅ Server is running"));
 app.use("/api/inngest", serve({ client: inngest, functions }));
-app.use("/api/user",userRouter)
+app.use("/api/user", userRouter);
+app.use("/api/post", postRouter);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () =>
